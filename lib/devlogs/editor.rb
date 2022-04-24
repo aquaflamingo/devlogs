a96a887f7b4894a90c6dfe778efc51833178b9fb
aquaflamingo <16901597+aquaflamingo@users.noreply.github.com>
2022-04-23 18:38:47 -0700

    Adds Editor

---
+++ lib/devlogs/editor.rb
@@ -0,0 +1,23 @@
+# Wrapper for terminal editor
+class Editor
+  def initialize
+    @program = ENV["EDITOR"]
+  end
+
+  # Opens the file contained at the path
+  def open(path)
+    command = "#{@program} #{path}"
+
+    system command
+  end
+
+  class << self
+
+    # Opens the file at +path+ using system editor
+    def open(path)
+      session = new
+
+      session.open(path)
+    end
+  end
+end
