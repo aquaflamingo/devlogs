9197c08c363b4dab60a21aedefda7bc8e1e211d0
aquaflamingo <16901597+aquaflamingo@users.noreply.github.com>
2022-09-18 18:12:10 -0400

    Adds TimeHelper

---
+++ lib/devlogs/helpers/time_helper.rb
@@ -0,0 +1,9 @@
+module TimeHelper
+  # Example: 11-22-2022__13h43m
+  TIME_FORMAT_FILE_PREFIX = "%m-%d-%Y__%kh%Mm"
+
+  def current_time(format: TIME_FORMAT_FILE_PREFIX)
+    time = Time.new
+    time.strftime(format)
+  end
+end
